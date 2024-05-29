64d2de2583a32ea77deba8f38b5f108e20124b36
Yiftach Ziv <>
2024-05-29 21:54:48 +0300

    early PlayerMovement, collision
    added colliders to kitchen also made player movable within confines

--- CoolAAhProject/Assets/Scripts/CamFollow.cs
+++ CoolAAhProject/Assets/Scripts/CamFollow.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CamFollow : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] GameObject block1;
+    [SerializeField] GameObject block2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,6 @@ public class CamFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = new Vector3(Mathf.Clamp(player.transform.position.x, block1.transform.position.x, block2.transform.position.x), gameObject.transform.position.y, gameObject.transform.position.z);
     }
 }
---
+++ CoolAAhProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	Vector2 Movement = new Vector2 (0, 0);
+	[SerializeField] float PlayerSpeed = 5;
+	public float upperLimitY;
+	public float lowerLimitY;
+	public float upperLimitX;
+	public float lowerLimitX;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.W))
+		{
+
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x, Mathf.Clamp(gameObject.transform.position.y+ PlayerSpeed, lowerLimitY, upperLimitY), gameObject.transform.position.z);
+			//gameObject.GetComponent<SpriteRenderer>().sprite = back;
+			//GetComponent<AudioSource>().Play();
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x, Mathf.Clamp(gameObject.transform.position.y - PlayerSpeed, lowerLimitY, upperLimitY), gameObject.transform.position.z);
+			//gameObject.GetComponent<SpriteRenderer>().sprite = front;
+			//GetComponent<AudioSource>().Play();
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x - PlayerSpeed, lowerLimitX, upperLimitX), gameObject.transform.position.y, gameObject.transform.position.z);
+
+			//gameObject.GetComponent<SpriteRenderer>().sprite = left;
+			//GetComponent<AudioSource>().Play();
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x+ PlayerSpeed, lowerLimitX, upperLimitX), gameObject.transform.position.y, gameObject.transform.position.z);
+			//gameObject.GetComponent<SpriteRenderer>().sprite = right;
+			//GetComponent<AudioSource>().Play();
+		}
+		
+	}
+}
